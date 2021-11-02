f0586fe3a692fbf870f69216c56fbbbad77efad3
DouglasTucker <DouglasTucker@u.boisestate.edu>
2021-11-02 16:13:18 -0600

    added some needed settings for implementing maps. changed list to represent tour locations.

--- app/src/main/java/com/cs402/bsutour/MainActivity.kt
+++ app/src/main/java/com/cs402/bsutour/MainActivity.kt
@@ -12,9 +12,12 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var kRecyclerView: RecyclerView
-    val PracticeList = arrayListOf<String>("Mercury", "Venus","Earth","Mars", "Jupiter", "Saturn")
-    val SPracticeList = arrayListOf<Boolean>(false, false,false,false, false, false)
+    val PracticeList = arrayListOf<String>("1.\tStudent Union Building", "2.\tRec Center","3.\tAlbertsons Stadium","4.\tFirst-Year Village", "5.\tAlbertsons Library", "6.\tThe Quad","7.\tFriendship Bridge","8.\tInteractive learning Center","9.\tCenter for Visual Arts","10.\tMicron Business and Economics Building","11.\tB Plaza")
+    val SPracticeList = arrayListOf<Boolean>(false, false, false, false, false, false, false, false, false, false, false)
+   // val LPracticeList = arrayListOf<Boolean>( )
+
     var acount = 0;
     var joinstring = ""
     var initposition = 0;
@@ -33,99 +36,99 @@ class MainActivity : AppCompatActivity() {
 
         kRecyclerView.setAdapter(kadapter)
 
-        val button: Button = findViewById(R.id.addbutton)
-        //set on-click listener
-        button.setOnClickListener {
-            // build alert dialog
-            val dialogBuilder = AlertDialog.Builder(this)
-            val m_Text = ""
-            val input = EditText(this)
-            input.setHint("Enter Text")
-            input.inputType = InputType.TYPE_CLASS_TEXT
-            dialogBuilder.setView(input)
+//        val button: Button = findViewById(R.id.addbutton)
+//        //set on-click listener
+//        button.setOnClickListener {
+//            // build alert dialog
+//            val dialogBuilder = AlertDialog.Builder(this)
+//            val m_Text = ""
+//            val input = EditText(this)
+//            input.setHint("Enter Text")
+//            input.inputType = InputType.TYPE_CLASS_TEXT
+//            dialogBuilder.setView(input)
+//
+//                .setCancelable(false)
+//
+//                .setPositiveButton("Proceed", DialogInterface.OnClickListener { dialog, which ->
+//                    var m_Text = input.text.toString()
+//                    if (m_Text.isNullOrEmpty()) {
+//                        acount += 1
+//                        m_Text = "Item $acount"
+//                    }
+//                    SPracticeList.add(false)
+//                    PracticeList.add(m_Text)
+//                    kadapter.notifyDataSetChanged()
+//                })
+//
+//                .setNegativeButton("Cancel", DialogInterface.OnClickListener {
+//                        dialog, id -> dialog.cancel()
+//                })
+//
+//            val alert = dialogBuilder.create()
+//            alert.setTitle("Enter new item")
+//
+//            alert.show()
+//
+//        }
 
-                .setCancelable(false)
+//        val rembutton: Button = findViewById(R.id.rembutton)
+//        //set on-click listener
+//        rembutton.setOnClickListener {
+//            for (i in SPracticeList.size downTo 1) {
+//                if(SPracticeList[i-1]) {
+//                    SPracticeList.removeAt(i-1)
+//                    PracticeList.removeAt(i-1)
+//                }
+//            }
+//            kadapter.notifyDataSetChanged()
+//        }
 
-                .setPositiveButton("Proceed", DialogInterface.OnClickListener { dialog, which ->
-                    var m_Text = input.text.toString()
-                    if (m_Text.isNullOrEmpty()) {
-                        acount += 1
-                        m_Text = "Item $acount"
-                    }
-                    SPracticeList.add(false)
-                    PracticeList.add(m_Text)
-                    kadapter.notifyDataSetChanged()
-                })
-
-                .setNegativeButton("Cancel", DialogInterface.OnClickListener {
-                        dialog, id -> dialog.cancel()
-                })
-
-            val alert = dialogBuilder.create()
-            alert.setTitle("Enter new item")
-
-            alert.show()
-
-        }
-
-        val rembutton: Button = findViewById(R.id.rembutton)
-        //set on-click listener
-        rembutton.setOnClickListener {
-            for (i in SPracticeList.size downTo 1) {
-                if(SPracticeList[i-1]) {
-                    SPracticeList.removeAt(i-1)
-                    PracticeList.removeAt(i-1)
-                }
-            }
-            kadapter.notifyDataSetChanged()
-        }
-
-        val joinbutton: Button = findViewById(R.id.joinbutton)
-        //set on-click listener
-        joinbutton.setOnClickListener {
-
-            for (i in SPracticeList.size downTo 1) {
-                if(SPracticeList[i-1]) {
-                    initposition = i-1
-                    joinstring = PracticeList.get(i-1).plus(", ").plus(joinstring)
-                    SPracticeList.removeAt(i-1)
-                    PracticeList.removeAt(i-1)
-                }
-            }
-            if (!joinstring.isNullOrEmpty()) {
-                joinstring = joinstring.dropLast(2)
-                SPracticeList.add(initposition, false)
-                PracticeList.add(initposition, joinstring)
-                joinstring = ""
-                initposition = 0;
-                kadapter.notifyDataSetChanged()
-            }
-
-            val splitbutton: Button = findViewById(R.id.splitbutton)
-            //set on-click listener
-            splitbutton.setOnClickListener {
-                var delimeter = ", "
-                for (i in SPracticeList.size downTo 1) { // find selected items
-                    if(SPracticeList[i-1]) { // if item selected
-                        var parts = PracticeList[i-1].split(delimeter) //list of parts
-
-                        if (!parts.isNullOrEmpty()){
-
-                            for(j in parts.size downTo 1){ //adding for each part
-                                SPracticeList.add(i, false)
-                                PracticeList.add(i, parts[j-1])
-                            }
-
-                            SPracticeList.removeAt(i-1)
-                            PracticeList.removeAt(i-1)
-                        }
-
-                    }
-                }
-                kadapter.notifyDataSetChanged()
-            }
-
-        }
+//        val joinbutton: Button = findViewById(R.id.joinbutton)
+//        //set on-click listener
+//        joinbutton.setOnClickListener {
+//
+//            for (i in SPracticeList.size downTo 1) {
+//                if(SPracticeList[i-1]) {
+//                    initposition = i-1
+//                    joinstring = PracticeList.get(i-1).plus(", ").plus(joinstring)
+//                    SPracticeList.removeAt(i-1)
+//                    PracticeList.removeAt(i-1)
+//                }
+//            }
+//            if (!joinstring.isNullOrEmpty()) {
+//                joinstring = joinstring.dropLast(2)
+//                SPracticeList.add(initposition, false)
+//                PracticeList.add(initposition, joinstring)
+//                joinstring = ""
+//                initposition = 0;
+//                kadapter.notifyDataSetChanged()
+//            }
+//
+//            val splitbutton: Button = findViewById(R.id.splitbutton)
+//            //set on-click listener
+//            splitbutton.setOnClickListener {
+//                var delimeter = ", "
+//                for (i in SPracticeList.size downTo 1) { // find selected items
+//                    if(SPracticeList[i-1]) { // if item selected
+//                        var parts = PracticeList[i-1].split(delimeter) //list of parts
+//
+//                        if (!parts.isNullOrEmpty()){
+//
+//                            for(j in parts.size downTo 1){ //adding for each part
+//                                SPracticeList.add(i, false)
+//                                PracticeList.add(i, parts[j-1])
+//                            }
+//
+//                            SPracticeList.removeAt(i-1)
+//                            PracticeList.removeAt(i-1)
+//                        }
+//
+//                    }
+//                }
+//                kadapter.notifyDataSetChanged()
+//            }
+//
+//        }
 
     }
 }
