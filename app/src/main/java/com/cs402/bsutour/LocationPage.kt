9ef84b798f91a9503fb0e3387826e94b233c53d7
DouglasTucker <DouglasTucker@u.boisestate.edu>
2021-11-18 19:02:33 -0700

    Audio player working, can play, pause, restart. Added audio files

--- app/src/main/java/com/cs402/bsutour/LocationPage.kt
+++ app/src/main/java/com/cs402/bsutour/LocationPage.kt
@@ -1,6 +1,7 @@
 package com.cs402.bsutour
 
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -9,19 +10,50 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.graphics.drawable.Drawable
+import android.media.MediaPlayer
+import android.util.Log
+import android.view.MotionEvent
+import kotlinx.android.synthetic.main.location_page_audio.*
 
 
-class LocationPage : AppCompatActivity()  {
+class LocationPage : AppCompatActivity() {
+    private var mediaPlayer: MediaPlayer? = null
 
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.location_page)
 
         val location = intent.getStringExtra("location_summary")
         val image = intent.getStringExtra("image")
         val latlong = intent.getStringExtra("latlong")
+        val ListIndex = intent.getIntExtra("listIndex", 0)
+        var toggle = 0
 
-        val textView = findViewById<TextView>(R.id.textView).apply{
+        if (ListIndex == 0) {
+            setContentView(R.layout.location_page_audio)
+        } else {
+            setContentView(R.layout.location_page)
+        }
+
+        Log.d("test1", "hello1")
+        val uria = "R.raw.$image" // where myresource (without the extension) is the file
+        Log.d("test1", "hello2")
+        val audioResource = resources.getIdentifier(uria, null, packageName)
+        Log.d("test1", "hello3")
+        mediaPlayer = MediaPlayer.create(this, R.raw.t1 )
+        Log.d("test1", "hello4")
+        mediaPlayer?.setOnPreparedListener {
+            println("READY TO GO")
+        }
+        Log.d("test1", "hello5")
+ /*       pushButton.setOnTouchListener { _, event ->
+            handleTouch(event)
+            true
+        }
+*/
+
+        val textView = findViewById<TextView>(R.id.textView).apply {
             text = location
         }
 
@@ -37,10 +69,74 @@ class LocationPage : AppCompatActivity()  {
 
         // Move to previous activty
         backButton.setOnClickListener { v: View? ->
+            if(toggle == 0){
+                mediaPlayer?.seekTo(0)
+                mediaPlayer?.pause()
+            }
             finish()
 
         }
 
+        if (ListIndex == 0) {
+            var playButton = findViewById(R.id.pushButton) as Button
+            playButton.setOnClickListener {
+                if(toggle == 0 ){
+                    toggle = 1
+                    mediaPlayer?.start()
+                    playButton.text = "pause"
+                } else{
+                    toggle = 0
+                    playButton.text = "start"
+                    mediaPlayer?.pause()
+                }
+            }
+
+            var restartButton = findViewById(R.id.restart_button) as Button
+            restartButton.setOnClickListener {
+                toggle = 1
+                playButton.text = "pause"
+                mediaPlayer?.seekTo(0)
+                mediaPlayer?.start()
+            }
+        }
 
     }
+
+
+
+
+/*
+    private fun handleTouch(event: MotionEvent?) {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                println("down")
+                mediaPlayer?.start()
+            }
+            MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP -> {
+                println("up or cancel")
+                mediaPlayer?.pause()
+                mediaPlayer?.seekTo(0)
+            }
+            else -> {
+                println("other")
+            }
+        }
+    }*/
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
